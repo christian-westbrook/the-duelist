fa0fd431f293126fd826703a3ba3c9593352eba6
christian-westbrook <christianwestbrook@live.com>
2024-08-15 18:59:44 -0500

    Example of window creation with SDL2 in C++

--- client/src/game.cpp
+++ client/src/game.cpp
@@ -1,12 +1,16 @@
-#include "SDL2/SDL.h"
-#include <iostream>
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-int main( int argc, char* args[] )
+#include "SDL2/SDL.h"
+
+int main(int argc, char* args[])
 {
-    //Start SDL
-    SDL_Init( SDL_INIT_EVERYTHING );
+    // Start SDL
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window *window = SDL_CreateWindow("The Duelist", 500, 500, 500, 500, 0);
     
-    //Quit SDL
+    // Quit SDL
     SDL_Quit();
     
     return 0;    
