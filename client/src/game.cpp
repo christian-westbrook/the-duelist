f5cb1885828e382136bd61a7ffa757227f98c7ce
christian-westbrook <christianwestbrook@live.com>
2024-08-15 21:24:53 -0500

    Example of game loop and input handling for closing the window

--- client/src/game.cpp
+++ client/src/game.cpp
@@ -4,6 +4,8 @@
 // Abstract    : This source file contains the entry point for the The Duelist.
 // -----------------------------------------------------------------------------
 
+// Imports
+#include "SDL2/SDL.h"
 
 // Define game constants
 #define GAME_TITLE "The Duelist"
@@ -13,9 +15,9 @@
 #define WINDOW_POS_Y 200
 #define WINDOW_WIDTH 500
 #define WINDOW_HEIGHT 500
-#define SDL_WINDOW_FLAGS 0
 
-#include "SDL2/SDL.h"
+#define SDL_WINDOW_FLAGS 0
+#define SDL_RENDERER_FLAGS 0
 
 int main(int argc, char* args[])
 {
@@ -33,6 +35,34 @@ int main(int argc, char* args[])
     // Create the SDL Renderer
    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_FLAGS);
 
+    // Create the game loop
+    bool is_running = true;
+
+    while(is_running) {
+        // Process input
+        SDL_Event event;
+        SDL_PollEvent(&event);
+
+        switch(event.type) {
+            case SDL_QUIT: {
+                is_running = false;
+                break;
+            }
+            case SDL_KEYDOWN: {
+                if(event.key.keysym.sym == SDLK_ESCAPE) {
+                    is_running = false;
+                    break;
+                }
+            }
+            default: {
+                break;
+            }
+        }
+        
+        // Tick
+
+        // Render
+    }
     
     // Quit SDL
     SDL_Quit();
