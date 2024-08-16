24f7556959a1170e7b1e7c953554e31b21d7e377
christian-westbrook <christianwestbrook@live.com>
2024-08-15 22:30:33 -0500

    Improved formatting and comments around event handling in game.cpp

--- client/src/game.cpp
+++ client/src/game.cpp
@@ -35,33 +35,39 @@ int main(int argc, char* args[])
     // Create the SDL Renderer
    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_FLAGS);
 
-    // Create the game loop
+    // -------------------------------------------------------------------------
+    // GAME LOOP
+    // -------------------------------------------------------------------------
     bool is_running = true;
-
+    
     while(is_running) {
-        // Process input
+        // ---------------------------------------------------------------------
+        // HANDLE EVENTS
+        // ---------------------------------------------------------------------
         SDL_Event event;
-        SDL_PollEvent(&event);
+        SDL_PollEvent(&event); // Store data from the latest event at the memory
+                               // address of the 'event' variable
 
         switch(event.type) {
-            case SDL_QUIT: {
+            case SDL_QUIT: // The user clicked on the exit button
                 is_running = false;
                 break;
-            }
-            case SDL_KEYDOWN: {
+            case SDL_KEYDOWN: // The user pressed a key
                 if(event.key.keysym.sym == SDLK_ESCAPE) {
                     is_running = false;
                     break;
                 }
-            }
-            default: {
+            default:
                 break;
-            }
         }
         
-        // Tick
+        // ---------------------------------------------------------------------
+        // TICK
+        // ---------------------------------------------------------------------
 
-        // Render
+        // ---------------------------------------------------------------------
+        // RENDER
+        // ---------------------------------------------------------------------
     }
     
     // Quit SDL
