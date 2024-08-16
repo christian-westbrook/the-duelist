5e40c07f933fc82b6560bdf9a8276653668b1aeb
christian-westbrook <christianwestbrook@live.com>
2024-08-15 21:06:08 -0500

    Moved magic numbers and strings used for creating the SDL window into constants

--- client/src/game.cpp
+++ client/src/game.cpp
@@ -1,6 +1,17 @@
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
 
+
+// Define game constants
+#define GAME_TITLE "The Duelist"
+
+// Define window constants
+#define WINDOW_POS_X 700
+#define WINDOW_POS_Y 200
+#define WINDOW_WIDTH 500
+#define WINDOW_HEIGHT 500
+#define SDL_WINDOW_FLAGS 0
+
 #include "SDL2/SDL.h"
 
 int main(int argc, char* args[])
@@ -8,7 +19,14 @@ int main(int argc, char* args[])
     // Start SDL
     SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_Window *window = SDL_CreateWindow("The Duelist", 500, 500, 500, 500, 0);
+    SDL_Window *window = SDL_CreateWindow(
+        GAME_TITLE, 
+        WINDOW_POS_X, WINDOW_POS_Y, 
+        WINDOW_WIDTH, WINDOW_HEIGHT, 
+        SDL_WINDOW_FLAGS
+    );
+
+    SDL_Delay(3000);
     
     // Quit SDL
     SDL_Quit();
