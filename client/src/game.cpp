6e4a6f1b339743e9ad3fc784062f0ab4f693ede2
christian-westbrook <christianwestbrook@live.com>
2024-08-15 22:37:28 -0500

    Minor linting edits

--- client/src/game.cpp
+++ client/src/game.cpp
@@ -22,48 +22,46 @@
 #define SDL_WINDOW_FLAGS 0
 #define SDL_RENDERER_FLAGS 0
 
-int main(int argc, char* args[])
-{
+int main(int argc, char* args[]) {
     // Initialize SDL
     SDL_Init(SDL_INIT_EVERYTHING);
 
     // Create the SDL Window
     SDL_Window *window = SDL_CreateWindow(
-        GAME_TITLE, 
-        WINDOW_POS_X, WINDOW_POS_Y, 
-        WINDOW_WIDTH, WINDOW_HEIGHT, 
-        SDL_WINDOW_FLAGS
-    );
+        GAME_TITLE,
+        WINDOW_POS_X, WINDOW_POS_Y,
+        WINDOW_WIDTH, WINDOW_HEIGHT,
+        SDL_WINDOW_FLAGS);
 
     // Create the SDL Renderer
-   SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_FLAGS);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_FLAGS);
 
     // -------------------------------------------------------------------------
     // GAME LOOP
     // -------------------------------------------------------------------------
     bool is_running = true;
-    
-    while(is_running) {
+
+    while (is_running) {
         // ---------------------------------------------------------------------
         // HANDLE EVENTS
         // ---------------------------------------------------------------------
         SDL_Event event;
-        SDL_PollEvent(&event); // Store data from the latest event at the memory
-                               // address of the 'event' variable
+        SDL_PollEvent(&event);  // Store data from the latest event at the
+                                // memory address of the 'event' variable
 
-        switch(event.type) {
-            case SDL_QUIT: // The user clicked on the exit button
+        switch (event.type) {
+            case SDL_QUIT:  // The user clicked on the exit button
                 is_running = false;
                 break;
-            case SDL_KEYDOWN: // The user pressed a key
-                if(event.key.keysym.sym == SDLK_ESCAPE) {
+            case SDL_KEYDOWN:  // The user pressed a key
+                if (event.key.keysym.sym == SDLK_ESCAPE) {
                     is_running = false;
                     break;
                 }
             default:
                 break;
         }
-        
+
         // ---------------------------------------------------------------------
         // TICK
         // ---------------------------------------------------------------------
@@ -72,9 +70,9 @@ int main(int argc, char* args[])
         // RENDER
         // ---------------------------------------------------------------------
     }
-    
+
     // Quit SDL
     SDL_Quit();
-    
+
     return 0;
 }
